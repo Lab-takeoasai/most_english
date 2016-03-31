5256078ec8a88bacabac1db58ca06de83e3059cb
Takeo Asai <spitson+github@gmail.com>
2016-03-31 18:13:33 +0900

    gitignore & validation

--- src/check.ts
+++ src/check.ts
@@ -16,11 +16,15 @@ class Checker {
     validates(text: string) {
         let errors = [];
         const lines = text.split("\n");
-        lines.forEach(l => {
-            const words = l.split(" ");
-            words.forEach(w => {
-                if (!this.contains(w)) {
-                    // errors.push({line: l});
+        lines.forEach((line, lineIndex) => {
+            let wordIndex = 0;
+            const words = line.split(" ");
+            words.forEach(word => {
+                if (!this.contains(word)) {
+                    wordIndex = line.indexOf(word, wordIndex);
+                    errors.push({word: word, lineIndex: lineIndex, wordIndex: wordIndex});
+                } else {
+                    wordIndex = line.indexOf(word, wordIndex);
                 }
             });
         });
@@ -29,10 +33,10 @@ class Checker {
 }
 
 let c = new Checker("./1-1000.txt");
-const txt = "this document is an example for top 1000 words used in english";
+const txt = "this document is an example for top  1000 words used in English";
 
 console.time("validates");
-for (let i = 0; i < 1000000; i++) {
-    c.validates(txt);
+for (let i = 0; i < 1; i++) {
+    console.log( c.validates(txt) );
 }
 console.timeEnd("validates");
